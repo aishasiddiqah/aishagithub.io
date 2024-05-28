617b589c8572641627c014d4d01609cee9ec2d7c
aishasiddiqah <aishasalam205@gmail.com>
2024-05-29 00:25:46 +0400

    Update home.js

--- home/home.js
+++ home/home.js
@@ -1,22 +1,22 @@
-//to change the header when the page is scrolled
+//function to change the header when the page is scrolled
 document.addEventListener('scroll', () => {
   const header = document.querySelector('header');
 
-  if (window.scrollY > 0){
+  if (window.scrollY > 0){ //the header will change once the user scrolls the page
       header.classList.add('change')
   }
-  else{
+  else{ //the header will return back once the user scrolls back up
       header.classList.remove('change')
   }
 }
 );
 
-
+//function for scrolling all the way to the top by pressing a button
 function goBackUp() {
   window.scrollTo(0,0);
 }
 
-//dark mode
+//function for enabling and disabling dark mode
 function darkMode() {
   var element = document.body;
   element.classList.toggle("dark");
