379db6601838fb516ceb3f9d58d9cb354528ef44
aishasiddiqah <aishasalam205@gmail.com>
2024-05-29 00:25:49 +0400

    Update gallery.js

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -1,23 +1,48 @@
+//function to change the header when the page is scrolled
+document.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+  
+    if (window.scrollY > 0){ //the header will change once the user scrolls the page
+        header.classList.add('change')
+    }
+    else{ //the header will return back once the user scrolls back up
+        header.classList.remove('change')
+    }
+}
+);
+  
+  //function for scrolling all the way to the top by pressing a button
+function goBackUp() {
+    window.scrollTo(0,0);
+}
+  
+  //function for enabling and disabling dark mode
+function darkMode() {
+    var element = document.body;
+    element.classList.toggle("dark");
+}
+
+//for the popup images in the gallery (Lightbox)
 var slideIndex = 1;
 showSlide(slideIndex);
 
-function openLightbox() {
+function openLightbox() { //function for enabling the lighbox (the popup wil appear)
   document.getElementById('Lightbox').style.display = 'block';
 }
 
-function closeLightbox() {
+function closeLightbox() { //function for closing the lighbox (the popup wil disappear)
   document.getElementById('Lightbox').style.display = 'none';
 }
 
-function changeSlide(n) { 
+function changeSlide(n) { //function for switching between slides
 	showSlide(slideIndex += n);
 }
 
-function toSlide(n) {
+function toSlide(n) { //function for going to the next slide
 	showSlide(slideIndex = n);
 }
 
-function showSlide(n) {
+function showSlide(n) { //function detailing the location of the next slide and the previous slide
 
   const slides = document.getElementsByClassName('slide');
   let modalPreviews = document.getElementsByClassName('modal-preview');
