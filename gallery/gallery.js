62cd08a2b1d6a9361bc005cb69519eb3f86a5b83
aishasiddiqah <aishasalam205@gmail.com>
2024-05-28 21:10:31 +0400

    Update gallery.js

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -1,42 +1,42 @@
-var slideIndex = 1;
-showSlide(slideIndex);
+var index = 1;
+show(index);
 
-function openLightbox() { //CHANGE LIGHTBOX TO GALLERY
-  document.getElementById('Lightbox').style.display = 'block';
+function open() { //CHANGE LIGHTBOX TO GALLERY
+  document.getElementById('lightbox').style.display = 'block';
 }
 
-function closeLightbox() {
-  document.getElementById('Lightbox').style.display = 'none';
+function close() {
+  document.getElementById('lightbox').style.display = 'none';
 }
 
-function changeSlide(n) { // CHANGE NAME TO CURRENT SLIDE 
-	showSlide(slideIndex += n);
+function change(n) { // CHANGE NAME TO CURRENT SLIDE 
+	show(index += n);
 }
 
-function toSlide(n) { //NEXT SLIDE
-	showSlide(slideIndex = n);
+function nextSlide(n) { //NEXT SLIDE
+	show(index = n);
 }
 
-function showSlide(n) {
+function show(n) {
 
   const slides = document.getElementsByClassName('slide');
-  let modalPreviews = document.getElementsByClassName('modal-preview');
+  let largePreviews = document.getElementsByClassName('large-preview');
 
   if (n > slides.length) {
-    slideIndex = 1;	
+    index = 1;	
   }
   
   if (n < 1) {
-  	slideIndex = slides.length;
+    index = slides.length;
   }
 
   for (let i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  for (let i = 0; i < modalPreviews.length; i++) {
-      modalPreviews[i].className = modalPreviews[i].className.replace(' active', '');
+  for (let i = 0; i < largePreviews.length; i++) {
+      largePreviews[i].className = largePreviews[i].className.replace(' active', '');
   }
   
-  slides[slideIndex - 1].style.display = 'block';
-  modalPreviews[slideIndex - 1].className += ' active';
+  slides[index - 1].style.display = 'block';
+  largePreviews[index - 1].className += ' active';
 }
